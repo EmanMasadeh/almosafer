b82f95d073dde2f97f27cd17bd99c7f9aa1fbdc0
EmanMasadeh <eman.abdullah.m@gmail.com>
2024-06-10 22:52:51 +0300

    update2 and i added before test and test

---
+++ src/myTestCases.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTestCases {
+	
+	@BeforeTest
+	public void mySetUp () {}
+	
+	@Test 
+	public void firstTest() {}
+	
+
+}
