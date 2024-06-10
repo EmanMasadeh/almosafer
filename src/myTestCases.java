a5e8c69913a22f57c321383b8bbc1ac54ee474f0
EmanMasadeh <eman.abdullah.m@gmail.com>
2024-06-10 22:57:15 +0300

    update3 add hello to myfirsttest

--- src/myTestCases.java
+++ src/myTestCases.java
@@ -1,13 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class myTestCases {
 	
+	WebDriver driver = new ChromeDriver();
+	
+	String URL="https://global.almosafer.com/en";
 	@BeforeTest
-	public void mySetUp () {}
+	public void mySetUp () {
+		
+		driver.get(URL);
+		
+		}
+	
+	
 	
 	@Test 
-	public void firstTest() {}
+	public void firstTest() {
+		
+		System.out.println("hello");
+	}
 	
 
 }
